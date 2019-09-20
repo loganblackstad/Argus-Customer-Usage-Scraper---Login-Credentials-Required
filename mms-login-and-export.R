5842e642f67a2bbd09e596fbdf3c48d3d35fe909
Logan Blackstad <logan.blackstad@gmail.com>
2019-09-20 15:12:12 -0500

    Completed Metal Management Usage Export
    Completed Metal Management Usage Export on Sep 20 2019
    
    Credit to:
    Jeff Taylor - Forward Curves Analyst, Argus Media
    Logan Blackstad - Metals BD, Argus Media

--- mms-login-and-export.R
+++ mms-login-and-export.R
@@ -1,13 +1,14 @@
 
   library(RSelenium)
+  library(KeyboardSimulator)
   
-  
+  rm(list=ls())
   
   userList <- read.csv("C:/Users/logan.blackstad/Desktop/mms-test-export.csv")
  
   
   
-  driver <- rsDriver(browser = c("firefox"))
+  driver <- rsDriver(verbose = FALSE,port=4444L, browser = c("firefox"))
   remote_driver <- driver[["client"]]
   
   #fprof <- makeFirefoxProfile(list(browser.helperApps.neverAsk.saveToDisk = "text/plain,text/x-csv,text/csv,application/vnd.ms-excel,application/csv,application/x-csv,text/csv,text/comma-separated-values,text/x-comma-separated-values,text/tab-separated-values,application/pdf"))
@@ -22,12 +23,12 @@
   
   username_element <- remote_driver$findElement(using = "id", value = "Username")
   
-  username_element$sendKeysToElement(list("###USERNAME###"))
+  username_element$sendKeysToElement(list(" %%USERNAME%% "))
   
   
   password_element <- remote_driver$findElement(using = "id", value = "Password")
   
-  password_element$sendKeysToElement(list("###PASSWORD###"))
+  password_element$sendKeysToElement(list(" %%PASSWORD%% "))
   
   login_element <- remote_driver$findElement(using = "id", value = "buttonSubmitLogin")
   login_element$clickElement()
@@ -43,7 +44,10 @@
   submit_element$clickElement()
   
   keybd.press('Alt+s')
+  Sys.sleep(1)
   keybd.press('enter')
+  Sys.sleep(2)
+  
   }
   
   
@@ -53,19 +57,65 @@
   }
   
   
-  Pull_UsageData("trisha.dooley")
+
   
-  
-  # loop through csv by username
-  
-  
-  
+  # generate subscriber list file
   # append csv files
   # add Username and company 
-  # save file
+  # save file to Desktop
   
   
-  
-  
-# driver$server$stop() 
+driver$server$stop()
 
+
+
+files <- list.files(path = "C:/Users/logan.blackstad/Downloads", pattern ="._2019-User_ ", full.names = TRUE)
+
+curFile <-  read.csv(files[1], header = TRUE)
+
+
+i <- 1
+curFile <-  read.csv(files[i], header = TRUE)
+myDF = curFile[FALSE,]
+
+for (myFile in files) {
+  curFile <-  read.csv(myFile, header = TRUE)
+  if(nrow(curFile) == 0)
+  {
+    # add a row that keeps username and detail but adds "No Usage Data" in the column "Last.Visit" 
+    curFile[nrow(curFile) + 1,] = "No Usage Data"
+  }
+  curFile["UserName"] <- userList$UserName[i]
+  curFile["Company"] <- userList$Company[i]
+  curFile["First"] <- userList$First.Name[i]
+  curFile["Last"] <- userList$Last.Name[i]
+  curFile["Email"] <- userList$Email.Address[i]
+  curFile["IsPrimary"] <- userList$IsPrimary[i]
+  
+  myDF <- rbind(myDF,curFile)
+  i <- i+1
+
+}
+
+# myDF = curFile[FALSE,]
+
+
+
+outputFolderPath <- "C:/Users/logan.blackstad/Desktop/"
+outputFileName <- paste0("Usage_Data_", userList$Company[1], ".csv", sep="")
+
+write.csv(myDF, file = paste0(outputFolderPath, outputFileName, sep=""), row.names=FALSE)
+
+# View(files)
+
+
+
+
+# Delete downloaded files from Downloads Folder
+
+for (myFile in files) {
+file.remove(myFile)
+}
+
+# clear console
+cat("\014")  
